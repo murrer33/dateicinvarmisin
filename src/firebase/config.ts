affde6b65f5b84f61b2fb6faa1680e10c270fd93
Berke Mehmet Utuş <berkeeutus@gmail.com>
2025-11-23 18:11:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-771844373-40307",
+  "appId": "1:722345478030:web:f0568d001f9f250a4786df",
+  "apiKey": "AIzaSyAtcG8lN0Esr1OxrTcLMxo8hvIse8HaEqE",
+  "authDomain": "studio-771844373-40307.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "722345478030"
+};
